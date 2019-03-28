3f13c00091e727246eef14eccba5de8073cebd48
you <you@examp.es>
2019-03-28 16:48:56 +0100

    Punto 3, sorpresa no va

--- app/src/main/java/com/example/cuatroenraya/activities/RoundListActivity.kt
+++ app/src/main/java/com/example/cuatroenraya/activities/RoundListActivity.kt
@@ -3,16 +3,14 @@ package com.example.cuatroenraya.activities
 import android.content.Intent
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
-import android.support.v7.widget.DefaultItemAnimator
-import android.support.v7.widget.LinearLayoutManager
-import com.example.cuatroenraya.R
 import com.example.cuatroenraya.model.RoundRepository
 import kotlinx.android.synthetic.main.activity_round_list.*
 import android.support.design.widget.Snackbar
 import com.example.cuatroenraya.model.Round
 import com.example.cuatroenraya.utility.executeTransaction
+import com.example.cuatroenraya.R
 
-class RoundListActivity : AppCompatActivity() {
+class   RoundListActivity : AppCompatActivity() {
     fun onRoundSelected(round: Round) {
         val intent = Intent(this, Ingame::class.java)
         intent.putExtra("STRINGTABLERO",round.stringTablero)
@@ -22,7 +20,7 @@ class RoundListActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_fragment)
+        setContentView(R.layout.activity_masterdetail)
         val fm = supportFragmentManager
         if (fm.findFragmentById(R.id.fragment_container) == null) {
             fm.executeTransaction { add(R.id.fragment_container, RoundListFragment()) }
