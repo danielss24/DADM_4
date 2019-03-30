cc71f3f57f37f6b0c6a38ff6f504bbd0249e91fd
LucasR8 <aspirina1512@gmail.com>
2019-03-30 22:59:00 +0100

    Boton de reinicio

--- app/src/main/java/com/example/cuatroenraya/activities/RoundFragment.kt
+++ app/src/main/java/com/example/cuatroenraya/activities/RoundFragment.kt
@@ -3,11 +3,13 @@ package com.example.cuatroenraya.activities
 import android.content.Context
 import android.net.Uri
 import android.os.Bundle
+import android.support.design.widget.FloatingActionButton
 import android.support.design.widget.Snackbar
 import android.support.v4.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ImageButton
 
 import com.example.cuatroenraya.R
 import com.example.cuatroenraya.model.JugadorConecta4
@@ -122,6 +124,91 @@ class RoundFragment : Fragment(), PartidaListener {
     }
 
     /**
+     * @brief registra clickers para fragmentos
+     * @param jugador jugador local
+     */
+    private fun registerListeners(jugador: View.OnClickListener) {
+        val ids = arrayOf(
+            intArrayOf(
+                R.id.c11,
+                R.id.c12,
+                R.id.c13,
+                R.id.c14,
+                R.id.c15,
+                R.id.c16,
+                R.id.c17
+            ),
+            intArrayOf(
+                R.id.c21,
+                R.id.c22,
+                R.id.c23,
+                R.id.c24,
+                R.id.c25,
+                R.id.c26,
+                R.id.c27
+            ),
+            intArrayOf(
+                R.id.c31,
+                R.id.c32,
+                R.id.c33,
+                R.id.c34,
+                R.id.c35,
+                R.id.c36,
+                R.id.c37
+            ),
+            intArrayOf(
+                R.id.c41,
+                R.id.c42,
+                R.id.c43,
+                R.id.c44,
+                R.id.c45,
+                R.id.c46,
+                R.id.c47
+            ),
+            intArrayOf(
+                R.id.c51,
+                R.id.c52,
+                R.id.c53,
+                R.id.c54,
+                R.id.c55,
+                R.id.c56,
+                R.id.c57
+            ),
+            intArrayOf(
+                R.id.c61,
+                R.id.c62,
+                R.id.c63,
+                R.id.c64,
+                R.id.c65,
+                R.id.c66,
+                R.id.c67
+            )
+        )
+        var button: ImageButton
+        for (i in 0 until ids.size)
+            for (j in 0 until ids[i].size) {
+                button = view!!.findViewById<View>(ids[i][j]) as ImageButton
+                button.setOnClickListener(jugador)
+            }
+
+        val resetButton = view!!.findViewById(R.id.reset_round_fab) as FloatingActionButton
+        resetButton.setOnClickListener(View.OnClickListener {
+            if (round.board.getEstado() !== Tablero.EN_CURSO) {
+                Snackbar.make(view as View,
+                    R.string.round_already_finished, Snackbar.LENGTH_SHORT).show()
+                return@OnClickListener
+            }
+            var tableroTMP = TableroConecta4()
+            round.board.stringToTablero(tableroTMP.tableroToString())
+            //startRound()
+            listener?.onRoundUpdated()
+            view?.update(round)
+            Snackbar.make(view as View, R.string.round_restarted,
+                Snackbar.LENGTH_SHORT).show()
+        })
+    }
+
+    /**
      * @brief inicia la vista con todos los jugadores, tablero y clickers
      */
     internal fun startRound() {
@@ -133,7 +220,8 @@ class RoundFragment : Fragment(), PartidaListener {
         game = Partida(round.board, players)
         game.addObservador(this)
         localPlayer.setPartida(game)
-        view?.setPlayerAsOnClickListener(localPlayer)
+        //view?.setPlayerAsOnClickListener(localPlayer)
+        registerListeners(localPlayer)
         if (game.tablero.estado == Tablero.EN_CURSO)
             game.comenzar()
     }
