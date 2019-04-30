657177206fe88996777334708dad01a2ec295bad
dSerena <daniels_s_24@hotmail.com>
2019-04-30 18:23:06 +0200

    Fixeado añadir partida al finalizar esta. Fixeado BUGAZO de infinito player random.

--- app/src/main/java/com/example/cuatroenraya/activities/AlertDialogFragment.kt
+++ app/src/main/java/com/example/cuatroenraya/activities/AlertDialogFragment.kt
@@ -5,10 +5,16 @@ import android.app.Dialog
 import android.os.Bundle
 import android.support.v4.app.DialogFragment
 import android.content.DialogInterface
+import android.content.Intent
 import android.preference.PreferenceManager
+import android.support.design.widget.Snackbar
 import android.support.v7.app.AppCompatActivity
 import com.example.cuatroenraya.R
+import com.example.cuatroenraya.model.Round
 import com.example.cuatroenraya.model.RoundRepository
+import com.example.cuatroenraya.model.RoundRepositoryFactory
+import com.example.cuatroenraya.utility.update
+import kotlinx.android.synthetic.main.fragment_round_list.*
 
 /**
  * @brief Clase de alert para fragmentos
@@ -29,12 +35,30 @@ class AlertDialogFragment : DialogFragment() {
 
         alertDialogBuilder.setMessage(mensaje)
         alertDialogBuilder.setPositiveButton("Yes") { dialog, which ->
-            //            RoundRepository.addRound()
-            if (activity is RoundListActivity)
-                //activity.onRoundUpdated()
-            else
-                activity?.finish()
-            dialog.dismiss()
+            val round = Round()
+            round.firstPlayerName = "Random"
+            round.firstPlayerUUID = "Random"
+            round.secondPlayerName = SettingsActivity.getPlayerName(context!!)
+            round.secondPlayerUUID = SettingsActivity.getPlayerUUID(context!!)
+
+            val repository = RoundRepositoryFactory.createRepository(context!!)
+
+            val callback = object : RoundRepository.BooleanCallback {
+                override fun onResponse(response: Boolean) {
+                    if (response == false) {
+                        //Snackbar.make(findViewById(R.id.recyclerView),"R.string.error_adding_round", Snackbar.LENGTH_LONG).show()
+                    } else {
+                       /* Snackbar.make(findViewById(R.id.recyclerView),"New " + round.title + " added", Snackbar.LENGTH_LONG).show()
+                        val fragmentManager = supportFragmentManager
+                        val roundListFragment =fragmentManager.findFragmentById(R.id.fragment_container)as RoundListFragment
+                        roundListFragment.recyclerView.update(SettingsActivity.getPlayerUUID(baseContext), { round -> onRoundSelected(round) }
+                        )*/
+                    }
+                }
+            }
+            repository?.addRound(round, callback)
+            val intent = Intent(context!!, RoundListActivity::class.java)
+            startActivity(intent)
         }
         alertDialogBuilder.setNegativeButton("No",
             object : DialogInterface.OnClickListener {
